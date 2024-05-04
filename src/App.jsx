a47207a5498bfc7c3ab48e10ba3d830be38df14d
Gregory Oghenah <kenny@gregorys-mbp.home>
2024-05-04 01:12:42 +0100

    Started the Projects component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
-import './App.css'
-import Header from './components/Header'
-import About from './components/About'
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import About from './components/About';
+import Projects from './components/Projects';
+//import Contact from './components/Contact'
 
 function App() {
 
@@ -9,6 +11,7 @@ function App() {
     <div>
       <Header />
       <About />
+      <Projects />
     </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
+import dns from 'dns'
 import react from '@vitejs/plugin-react'
+
+dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    strictPort: true
  },
 })
